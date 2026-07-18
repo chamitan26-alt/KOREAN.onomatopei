0ffe4b06d8353942c6ea45eb1f88ce235d70e444
chamitan26-alt <chamitan26@gmail.com>
2026-07-18 15:18:23 +0900

    Update completion message in quiz area

--- script.js
+++ script.js
@@ -156,7 +156,8 @@ nextButton.onclick = function() {
             <h2>🎉終了！</h2>
             <p>得点：${score} / 10</p>
             <p style="color: #4a5568; padding: 10px; font-weight: bold; font-size: 1.1em;">
-                🐶「수고하셨습니다!（スゴハショッスンニダ！お疲れ様でした！）」
+                🐶「수고하셨습니다! <br>
+                （スゴハショッスンニダ！お疲れ様でした！）」
             </p>
             <br>
             <button id="retryButton">もう一度挑戦する</button>
